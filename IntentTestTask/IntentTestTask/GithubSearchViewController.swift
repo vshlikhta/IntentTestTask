d72042165d8503a5696b50ea12ba19fe361f7583
vshlikhta <vshlikhta@binariks.com>
2021-06-08 17:48:30 +0300

    Initial commit

---
+++ IntentTestTask/IntentTestTask/GithubSearchViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IntentTestTask
+//
+//  Created by Volodymyr Shlikhta on 08.06.2021.
+//
+
+import UIKit
+
+class GithubSearchViewController: UIViewController {
+
+    @IBOutlet weak var searchTextfield: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var searchResultsTableView: UITableView!
+    
+    @IBAction func didTapSearchButton(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
