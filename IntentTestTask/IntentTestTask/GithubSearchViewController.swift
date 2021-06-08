ba912c4274aa14c78f59684e9a1d7bf13c31788d
vshlikhta <vshlikhta@binariks.com>
2021-06-08 17:49:45 +0300

    added basic ui for search cells and main screen

--- IntentTestTask/IntentTestTask/GithubSearchViewController.swift
+++ IntentTestTask/IntentTestTask/GithubSearchViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class GithubSearchViewController: UIViewController {
 
+    // MARK: - Outlets
+    
     @IBOutlet weak var searchTextfield: UITextField!
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var searchResultsTableView: UITableView!
     
+    // MARK: - Actions
+    
     @IBAction func didTapSearchButton(_ sender: UIButton) {
         
     }
+    
+    // MARK: - View lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ IntentTestTask/IntentTestTask/SearchResultTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  SearchResultTableViewCell.swift
+//  IntentTestTask
+//
+//  Created by Volodymyr Shlikhta on 08.06.2021.
+//
+
+import UIKit
+
+struct SearchResultViewModel {
+    let id: String
+    let title: String
+    let description: String
+    let language: String
+    let isPrivate: Bool
+    let licenseDescription: String
+}
+
+//{
+//  "total_count": 40,
+//  "incomplete_results": false,
+//  "items": [
+//    {
+//      "id": 3081286,
+//      "full_name": "dtrupenn/Tetris",
+//      "private": false,
+//      "html_url": "https://github.com/dtrupenn/Tetris",
+//      "description": "A C implementation of Tetris using Pennsim through LC4",
+//      "language": "Assembly",
+//      "license": {
+//        "name": "MIT License",
+//      }
+//    }
+//  ]
+//}
+
+class SearchResultTableViewCell: UITableViewCell {
+    
+}
